ed64987deacb1c95b35d77bbeca3ffba6c761d48
Thomas <thomas.tricarico@gmail.com>
2013-08-14 16:12:06 -0400

    framework generally laid out access_token isn't sent back to facebook

--- Facebook.php
+++ Facebook.php
@@ -1,6 +1,136 @@
 <?php
 
+/***
+ * FacebookConnect - A simple way to connect and use the Facebook API
+ * @name FacebookConnect
+ * @author Thomas Tricarico
+ * @copyright (c) 2013 Thomas Tricarico
+ * @version 1.0a
+ ****/
+
 class Facebook {
 	
+	private static $instance;
+	private static $appid = '659774414034474';
+	private static $appsecret = '6dcb7f4d1cb488d627acd60b5b8b10aa';
+	private static $callbackurl = 'http://localhost/fb/';
+	private static $state; //also stored in session
+	private static $access_token;
+	private static $expiretime;
+	
+	private $user;
+	
+	/**
+	 * Here we are going to get the information from Facebook about the person that we are dealing with.
+	 */
+	public function __construct() {
+		
+		if(func_num_args() == 2) {
+			self::$access_token = $_SESSION['FacebookConnect']['access_token'] = func_get_arg(0);
+			self::$expiretime = $_SESSION['FacebookConnect']['expiretime'] = time() + func_get_arg(1);
+		}
+		else {
+			$access_token = $_SESSION['FacebookConnect']['access_token'];
+			$expiretime = $_SESSION['FacebookConnect']['expiretime'];
+		}
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/me?access_token='.self::$access_token);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$u = curl_exec($ch);
+		
+		if(!$u) {
+			throw new Exception(curl_error($ch));
+		}
+		
+		$this->user = json_decode($u);
+		
+	}
+	
+	/*
+	 * creates a state to help defend against CSRF
+	 * @param none
+	 * @return $state
+	 */
+	private static function createState() {
+		$s = mt_rand(0, mt_getrandmax());
+		$s = md5(time() . $s);
+		$s = substr($s, 0, 16);
+		$_SESSION['FacebookConnect']['state'] = $s;
+		self::$state = $s;
+		return $s;
+	}
+	
+	/*
+	 * checks to make sure the states are still okay
+	 * @param string $state
+	 * @return boolean
+	 */
+	private static function checkState($state) {
+		//if($_SESSION['FacebookConnect']['state'] == $state)
+			return true;
+		//else 
+		//	return false;
+	}
+	
+	/**
+	 * So we need to make the login over different functions, since the script
+	 * has to contact Facebook, get a response, then send the access code back
+	 */
+	public static function connectionPartOne() {
+		header('Location: https://www.facebook.com/dialog/oauth?client_id=659774414034474
+																&redirect_uri='. self::$callbackurl.'
+																&state='.self::createState());
+	}
+	public static function connectionPartTwo($finalcallback) {
+		//first, check state
+		if(!self::checkState($_REQUEST['state']))
+			throw new Exception("Possible CSRF Attack; Returned state does not equal saved state");
+			
+		
+
+		//check if error; throw exception, display error
+		if(isset($_REQUEST['error'])) {
+			$_REQUEST['error'];
+			$_REQUEST['error_code'];
+			$_REQUEST['error_description'];
+			$_REQUEST['error_reason'];
+		}
+		
+		
+		//check if code is sent back, if so, all okay!
+		if($_REQUEST['code'] != '') {
+			//do the final part of the connection
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/oauth/access_token?client_id=659774414034474'.
+																			'&redirect_uri='.self::$callbackurl.
+																			'&client_secret='.self::$appsecret.
+																			'&code='.$_REQUEST['code'].
+																			'&state='.self::createState());
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			$return = curl_exec($ch);
+			
+			if(!$return)
+				throw new Exception("There was a problem retrieving data from Facebook, CURL responded with: ".curl_error($ch));
+			
+			$return = explode('&', $return);
+			$access_token = explode('=', $return[0]);
+			$expires = explode('=', $return[1]);
+			new Facebook($access_token[1], $expires[1]);
+			
+			header('Location: '.$finalcallback);
+
+		}
+	}
 }
+
+function getFacebookLoginButton() {
+	if(!Facebook::getInstance())
+		echo '<a class="fbconnect" href="#"></a>';
+	else
+		echo 'login details';
+}
+
 
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include('Facebook.php');
+
+if(!isset($_REQUEST['code']) && !isset($_GET['finished']))
+	Facebook::connectionPartOne();
+
+elseif(!isset($_GET['finished']))
+	Facebook::connectionPartTwo('http://localhost/fb/?finished=1');
+
+else {
+	$fb = new Facebook();
+	var_dump($fb);
+}
+
